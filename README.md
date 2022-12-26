2c7540bc8a487c0225047b637f2541eda3089a1d
Sagar <macbook@Macbooks-MacBook-Pro.local>
2022-12-26 15:17:05 +0545

    Refreshed .gitignore

--- README.md
+++
@@ -1,3 +0,0 @@
-Min. PHP version required is 8
-1. composer install
-2. go to php ini, uncomment extension=gd & reset php service.
--- resources/js/plugins/storePlugins.js
+++
@@ -1,9 +0,0 @@
-// import { repositories } from "@/services/api";
-
-// export default function(store) {
-//   try {
-//     store.$api = repositories;
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
