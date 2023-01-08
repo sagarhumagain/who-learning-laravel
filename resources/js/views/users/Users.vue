2e1329333ad19c0485d1f55534631bff96897da8
Sagar <macbook@Macbooks-MacBook-Pro.local>
2023-01-09 01:58:06 +0545

    contract bug fix

--- resources/js/views/users/Users.vue
+++ resources/js/views/users/Users.vue
@@ -306,8 +306,8 @@
             <div class="form-group col-md-6 ">
                 <h6> Contract Status</h6>
                 <div class="form-control">
-                    <input v-model="form.is_active"  type="checkbox" name="is_active"  class="form-check-input mr-2" :class="{ 'is-invalid': form.errors.has('is_active') }">
-                    <label class="form-check-label">
+                    <input v-model="form.is_active" :true-value="1" :false-value="0" type="checkbox" name="is_active"  class="form-check-input mr-2" :class="{ 'is-invalid': form.errors.has('is_active') }">
+                    <label class="form-check-label" for="1">
                         Is Active
                     </label>
                     <error-msg :errors="errors" field="is_active"></error-msg>
@@ -441,7 +441,6 @@
                 this.editmode = true;
                 contract.is_active = contract.is_active == true ? 1 : 0;
                 const updatedContract = {...contract,pillar_id};
-                console.log(updatedContract)
                 $('#addNewContract').modal('show');
                 this.emitter.emit('editing', updatedContract);
             },
