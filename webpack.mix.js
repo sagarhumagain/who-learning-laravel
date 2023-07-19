f3e01968dc2190c717a07e2b397eb8f53a1f1f49
Sagar <macbook@Macbooks-MacBook-Pro.local>
2023-07-19 17:36:32 +0545

    webpack update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,28 +1,47 @@
-const mix = require('laravel-mix');
-const { VueLoaderPlugin } = require('vue-loader');
-
 require('dotenv').config();
 
-mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
-  .version()
-  .browserSync('http://127.0.0.1:8000')
+const mix = require('laravel-mix');
+const webpack = require('webpack');
+
+const dotenvplugin = new webpack.DefinePlugin({
+  'process.env': {
+    APP_NAME: JSON.stringify(process.env.APP_NAME || 'Default app name'),
+    NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    APP_URL: JSON.stringify(process.env.APP_URL || 'http://localhost:8000'),
+  },
+});
+
+mix
+  .webpackConfig({
+    plugins: [dotenvplugin],
+  })
   .webpackConfig({
     module: {
       rules: [
         {
-          test: /\.vue$/,
-          loader: 'vue-loader'
-        }
-      ]
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              appendTsSuffixTo: [/\.vue$/],
+            },
+          },
+          exclude: /node_modules/,
+        },
+      ],
     },
     resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue', '.json'],
+      extensions: ['.js', '.jsx', '.vue', '.ts', '.tsx', '.json'],
       alias: {
-        '@': __dirname + '/resources/js',
+        // 'vue$': 'vue/dist/vue.esm.js',
+        '@': `${__dirname}/resources/js`,
       },
     },
-    plugins: [
-      new VueLoaderPlugin()
-    ]
   });
+
+mix
+  .js('resources/js/app.js', 'public/js')
+  .vue({ version: 3 }) // Use the appropriate Vue version (e.g., Vue 2 or Vue 3)
+  .sass('resources/sass/app.scss', 'public/css')
+  .version()
+  .browserSync('http://127.0.0.1:8000');
