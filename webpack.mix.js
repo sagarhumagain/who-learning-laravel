d81d26de6b377d342789f98a8622e04b2e2ec2e5
Sagar <macbook@Macbooks-MacBook-Pro.local>
2023-07-19 17:29:36 +0545

    vue loader

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,28 @@
 const mix = require('laravel-mix');
+const { VueLoaderPlugin } = require('vue-loader');
 
 require('dotenv').config();
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .version()
-    .browserSync('http://127.0.0.1:8000')
-    .webpackConfig({
-        resolve: {
-            extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue', '.json'],
-            alias: {
-                '@': __dirname + '/resources/js',
-            },
-        },
-    });
+  .sass('resources/sass/app.scss', 'public/css')
+  .version()
+  .browserSync('http://127.0.0.1:8000')
+  .webpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue', '.json'],
+      alias: {
+        '@': __dirname + '/resources/js',
+      },
+    },
+    plugins: [
+      new VueLoaderPlugin()
+    ]
+  });
